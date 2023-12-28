004a1b40b056e7c7efca019d2ae1cef0426ffcbe
Eru <muhamadgilman999@gmail.com>
2023-12-28 09:29:08 +0700

    gg

--- src/script/prisma.ts
+++ src/script/prisma.ts
@@ -1,19 +1,23 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-    var prisma: PrismaClient | undefined;
+  var prisma: PrismaClient | undefined;
 }
 
 let prisma: PrismaClient;
 
-if (process.env.NODE_ENV === "production"){
-    prisma = new PrismaClient();
-}else {
-    if (!global.prisma){
-        global.prisma = new PrismaClient();
-    }
-
-    prisma = global.prisma;
+// Inisialisasi PrismaClient
+if (process.env.NODE_ENV === "production") {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
+} else {
+  // Jika bukan lingkungan produksi, inisialisasi baru setiap kali modul diimpor
+  if (!global.prisma) {
+    global.prisma = new PrismaClient();
+  }
+  prisma = global.prisma;
 }
 
 export default prisma;
